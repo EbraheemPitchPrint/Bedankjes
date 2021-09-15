8554a8e8b525c204f5e75e09f16f8c5daba29171
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-15 09:24:16 +0200

    added test request

--- custom.js
+++ custom.js
@@ -1,2 +1,9 @@
 // JavaScript source code
 
+    fetch('https://rasengan01.myshopify.com/admin/oauth/access_scopes.json', {
+        headers: {
+            'X-Shopify-Access-Token': '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',
+        },
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
