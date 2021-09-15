0f22cb01092ba13c8d7048f7b3ebac2e2403a377
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-15 14:22:37 +0200

    updated access token

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@
 
     fetch('https://rasengan01.myshopify.com/admin/oauth/access_scopes.json', {
         headers: {
-            'X-Shopify-Access-Token': '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',
+            'X-Shopify-Access-Token': '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',,
         },
     })
     .then(response => response.json())
