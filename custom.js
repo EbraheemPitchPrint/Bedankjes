abde35b572a9ce3e69da8294cd24f50ab9372ca7
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-15 13:44:10 +0200

    changed api key

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@
 
     fetch('https://rasengan01.myshopify.com/admin/oauth/access_scopes.json', {
         headers: {
-            'X-Shopify-Access-Token': 'e481375958f3313e5fb54e4090a4ba0b',
+            'X-Shopify-Access-Token': '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',
         },
     })
     .then(response => response.json())
