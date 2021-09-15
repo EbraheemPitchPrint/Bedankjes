db70af97876d8476c7c5b87b1ec5df4684afa7e8
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-15 14:23:47 +0200

    fixed comma error

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@
 
     fetch('https://rasengan01.myshopify.com/admin/oauth/access_scopes.json', {
         headers: {
-            'X-Shopify-Access-Token': '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',,
+            'X-Shopify-Access-Token': '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',
         },
     })
     .then(response => response.json())
