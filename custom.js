8e35c144362588932ab89b8cec173330665e9449
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-15 16:11:11 +0200

    update

--- custom.js
+++ custom.js
@@ -1,18 +1,18 @@
 // JavaScript source code
 
-var shop = 'rasengan01',
-    client_id = 'e481375958f3313e5fb54e4090a4ba0b',
-    scopes = 'read_customers, write_customers,read_inventory, read_orders, write_orders, write_inventory, read_merchant_managed_fulfillment_orders, write_merchant_managed_fulfillment_orders, read_assigned_fulfillment_orders, write_assigned_fulfillment_orders',
-    redirectUri = 'https://ebraheempitchprint.github.io/Bedankjes/custom.js',
-    nonce = 2000,
-    access_mode = 'value';
+//var shop = 'rasengan01',
+//    client_id = 'e481375958f3313e5fb54e4090a4ba0b',
+//    scopes = 'read_customers, write_customers,read_inventory, read_orders, write_orders, write_inventory, read_merchant_managed_fulfillment_orders, write_merchant_managed_fulfillment_orders, read_assigned_fulfillment_orders, write_assigned_fulfillment_orders',
+//    redirectUri = 'https://ebraheempitchprint.github.io/Bedankjes/custom.js',
+//    nonce = 2000,
+//    access_mode = 'value';
 
-window.location.href = `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${api_key}&scope=${scopes}&redirect_uri=${redirect_uri}&state=${nonce}&grant_options[]=${access_mode}`
+//window.location.href = `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${api_key}&scope=${scopes}&redirect_uri=${redirect_uri}&state=${nonce}&grant_options[]=${access_mode}`
 
-    fetch('https://rasengan01.myshopify.com/admin/oauth/access_scopes.json', {
-        headers: {
-            'X-Shopify-Access-Token': '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',
-        },
-    })
-    .then(response => response.json())
-    .then(data => console.log(data));
+//    fetch('https://rasengan01.myshopify.com/admin/oauth/access_scopes.json', {
+//        headers: {
+//            'X-Shopify-Access-Token': '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',
+//        },
+//    })
+//    .then(response => response.json())
+//    .then(data => console.log(data));
