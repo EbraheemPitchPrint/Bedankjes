b956a3852c5c05b2f065f0577cd18e58ae9237fb
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-16 14:34:24 +0200

    added api key

--- custom.js
+++ custom.js
@@ -3,12 +3,12 @@
 var shop = 'rasengan01',
     client_id = 'e481375958f3313e5fb54e4090a4ba0b',
     scopes = 'read_customers, write_customers,read_inventory, read_orders, write_orders, write_inventory, read_merchant_managed_fulfillment_orders, write_merchant_managed_fulfillment_orders, read_assigned_fulfillment_orders, write_assigned_fulfillment_orders',
-    redirectUri = 'https://ebraheempitchprint.github.io/Bedankjes/custom.js',
+    redirectUri = 'https://rasengan01.myshopify.com/admin/apps/bedankjes-custom-work-1',
     nonce = 2000,
     access_mode = 'value';
 
 if (localStorage.getItem("_i") === null) {
-    window.location.href = `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${api_key}&scope=${scopes}&redirect_uri=${redirect_uri}&state=${nonce}&grant_options[]=${access_mode}`;
+    window.location.href = `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${client_id}&scope=${scopes}&redirect_uri=${redirect_uri}&state=${nonce}&grant_options[]=${access_mode}`;
     localStorage.setItem('_i', true);
 } else {
     console.log(window.location.pathname);
