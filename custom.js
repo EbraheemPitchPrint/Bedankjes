9addea321917a6e2a063eee4c1ec46ea87d65f47
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-17 10:59:01 +0200

    get product

--- custom.js
+++ custom.js
@@ -1,11 +1,18 @@
 // JavaScript source code
 
-    var token = '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'
-    var baseUrl = 'https://rasengan01.myshopify.com'
-    fetch(baseUrl + '/admin/api/2021-07/products.json', {
+var token = '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'
+var baseUrl = 'https://rasengan01.myshopify.com'
+
+var productId = '';
+
+if (document.getElementById('id')) {
+    productId = document.getElementById('id').value;
+    fetch(baseUrl + `/admin/api/2021-07/products/${productId}.json`, {
         headers: {
-           'X-Shopify-Access-Token': token,
+            'X-Shopify-Access-Token': token,
         },
     })
     .then(response => response.json())
     .then(data => console.log(data));
+}
+
