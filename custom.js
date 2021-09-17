2f45bbe589378b6deb933752e6f7926a65a30a40
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-17 11:32:35 +0200

    added preview url as metadata

--- custom.js
+++ custom.js
@@ -2,19 +2,27 @@
 
 var token = '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'
 var baseUrl = 'https://rasengan01.myshopify.com'
-
+   
 setTimeout(() => {
     function getProjectData(_e) {
         console.log(_e);
+        fetch(baseUrl + `/admin/api/2021-07/products/${_e.data.source.product.id}/metafields.json`, {
+            headers: {
+                'X-Shopify-Access-Token': token,
+                'Accept': "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "metafield": {
+                    "namespace": "inventory",
+                    "key": "designImage",
+                    "value": `${data.previews[0]}`,
+                    "type": "single_line_text_field"
+                }
+            })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
     }
     window.ppclient.on('project-saved', getProjectData);
 }, 2000)
-
-
-fetch(baseUrl + `/admin/api/2021-07/metafields.json`, {
-    headers: {
-        'X-Shopify-Access-Token': token,
-    },
-})
-    .then(response => response.json())
-    .then(data => console.log(data));
