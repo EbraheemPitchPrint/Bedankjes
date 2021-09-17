9098adbda7c293a29df58bf844f7f7df528d37fa
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-17 11:07:42 +0200

    get metafields

--- custom.js
+++ custom.js
@@ -3,7 +3,7 @@
 var token = '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'
 var baseUrl = 'https://rasengan01.myshopify.com'
 
-fetch(baseUrl + `/admin/api/2021-07/products.json`, {
+fetch(baseUrl + `/admin/api/2021-07/metafields.json`, {
     headers: {
         'X-Shopify-Access-Token': token,
     },
