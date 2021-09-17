31e581dbfc0e1784c2e22052bffa78c731092ea8
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-17 11:19:13 +0200

    get project data

--- custom.js
+++ custom.js
@@ -3,21 +3,10 @@
 var token = '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'
 var baseUrl = 'https://rasengan01.myshopify.com'
 
-if (window.ppclient) {
-
-    function getProjectData(_e) {
-        console.log(_e);
-    }
-
-    window.ppDesignerInitialized = function (_class) {
-        if (_class.name === 'client') {
-            window.ppclient.on('project-saved', getProjectData);
-        } else if (_class.name === 'frame') {
-            // this was invoked by the frame
-        }
-    }
+function getProjectData(_e) {
+    console.log(_e);
 }
-
+window.ppclient.on('project-saved', getProjectData);
 fetch(baseUrl + `/admin/api/2021-07/metafields.json`, {
     headers: {
         'X-Shopify-Access-Token': token,
