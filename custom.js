d261646f5170beeee6dc9f67997375b907dada90
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-21 17:55:42 +0200

    test

--- custom.js
+++ custom.js
@@ -2,13 +2,14 @@
 
 (function (global) {
 	var token = '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'
-	var baseUrl = 'https://rasengan01.myshopify.com'
+	var baseUrl = 'https://rasengan01.myshopify.com/admin/api/2021-07/'
 
 	if (window.location.pathname.indexOf('/products/') !== -1) {
 		setTimeout(() => {
 			function getProjectData(_e) {
 				console.log(_e);
-				fetch(baseUrl + `/admin/api/2021-07/products/${_e.data.source.product.id}/metafields.json`, {
+				console.log(baseUrl + `/products/${ _e.data.source.product.id }/metafields.json`)
+				fetch(baseUrl + `products/${_e.data.source.product.id}/metafields.json`, {
 					method: 'POST',
 					headers: {
 						'X-Shopify-Access-Token': token,
