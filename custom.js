8ce26f4812ec8e6bf0a5bed1a541aaaacf219e00
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-23 09:15:56 +0200

    test

--- custom.js
+++ custom.js
@@ -9,21 +9,31 @@
 			function getProjectData(_e) {
 				console.log(_e);
 				console.log(baseUrl + `/products/${ _e.data.source.product.id }/metafields.json`)
-				fetch(baseUrl + `products/${_e.data.source.product.id}/metafields.json`, {
-					method: 'POST',
+				//fetch(baseUrl + `products/${_e.data.source.product.id}/metafields.json`, {
+				//	method: 'POST',
+				//	headers: {
+				//		'X-Shopify-Access-Token': token,
+				//		'Accept': "application/json",
+				//		"Content-Type": "application/json"
+				//	},
+				//	body: JSON.stringify({
+				//		"metafield": {
+				//			"namespace": "inventory",
+				//			"key": "designImage",
+				//			"value": `${_e.data.previews[0]}`,
+				//			"type": "single_line_text_field"
+				//		}
+				//	})
+				//})
+				//	.then(response => response.json())
+				//	.then(data => console.log(data));
+				fetch(baseUrl + `products/metafields.json`, {
+					method: 'GET',
 					headers: {
 						'X-Shopify-Access-Token': token,
 						'Accept': "application/json",
 						"Content-Type": "application/json"
 					},
-					body: JSON.stringify({
-						"metafield": {
-							"namespace": "inventory",
-							"key": "designImage",
-							"value": `${_e.data.previews[0]}`,
-							"type": "single_line_text_field"
-						}
-					})
 				})
 					.then(response => response.json())
 					.then(data => console.log(data));
