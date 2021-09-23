3ca068e6a204e24d054eb9290ae75808b92792d7
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-23 11:23:05 +0200

    test

--- custom.js
+++ custom.js
@@ -4,7 +4,13 @@
 	var token = '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'
 	var baseUrl = 'https://rasengan01.myshopify.com/admin/api/2021-07/'
 
+	var _cartForm = document.querySelector('[action="/cart/add"],[action="/cart/add.js"],#add-item-form,#add-to-cart-form');
+
 	if (window.location.pathname.indexOf('/products/') !== -1) {
+		if (_cartForm) {
+			_cartForm.insertAdjacentHTML('afterbegin', '<p class="line-item-property__field">< label for= "designurl" > designUrl</label ><input id="designurl" type="text" name="properties[_designUrl]"></p>');
+		};
+		
 		setTimeout(() => {
 			function getProjectData(_e) {
 				console.log(_e);
@@ -36,15 +42,4 @@
 			
 		}, 2000)
 	}
-
-	fetch(baseUrl + `events.json`, {
-		method: 'GET',
-		headers: {
-			'X-Shopify-Access-Token': token,
-			'Accept': "application/json",
-			"Content-Type": "application/json"
-		},
-	})
-		.then(response => response.json())
-		.then(data => console.log(data));
 }(this))
