175b295a2faa07c6e754d37e709c51174e9ae896
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-30 14:47:58 +0200

    update

--- custom.js
+++ custom.js
@@ -10,23 +10,43 @@
 		setTimeout(() => {
 			function setProjectId(_e) {
 				console.log(_e);
+				//console.log(baseUrl + `/products/${ _e.data.source.product.id }/metafields.json`)
+				//fetch(baseUrl + `products/${_e.data.source.product.id}/metafields.json`, {
+				//	method: 'POST',
+				//	headers: {
+				//		'X-Shopify-Access-Token': token,
+				//		'Accept': "application/json",
+				//		"Content-Type": "application/json"
+				//	},
+				//	body: JSON.stringify({
+				//		"metafield": {
+				//			"namespace": "inventory",
+				//			"key": "designImage",
+				//			"value": `${_e.data.previews[0]}`,
+				//			"type": "single_line_text_field"
+				//		}
+				//	})
+				//})
+				//	.then(response => response.json())
+				//	.then(data => console.log(data));
+
 				if (document.getElementById('_pitchprint')) {
 					var pp = document.getElementById('_pitchprint').value;
 					var design = document.getElementById('designurl');
-					design.value = 'https://s3.eu-west-1.amazonaws.com/pdf.pitchprint/' + pp + '.png';
+					design.value = `https://s3-eu-west-1.amazonaws.com/pitchprint.io/previews/${pp}_1.jpg`
 				}
 			}
-			//function setProjectId2(_e) {
-			//	console.log(_e);
+			function setProjectId2(_e) {
+				console.log(_e);
 
-			//	if (document.getElementById('designurl')) {
-			//		var design = document.getElementById('designurl');
-			//		design.value = _e.data.previews[0];
-			//	}
-			//}
+				if (document.getElementById('designurl')) {
+					var design = document.getElementById('designurl');
+					design.value = _e.data.previews[0];
+				}
+			}
 			if (window.ppclient) {
-				window.ppclient.on('project-saved', setProjectId);
-				window.ppclient.on('lib-ready', setProjectId);
+				window.ppclient.on('project-saved', setProjectId2);
+				ppclient.on('lib-ready', setProjectId);
 			}
 			
 		}, 2000)
