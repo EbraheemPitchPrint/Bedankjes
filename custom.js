821a396a1f1827f09dfe6c2aedcff81550176508
Ebraheem Seale <ebraheem@pitchprint.com>
2021-09-30 11:57:41 +0200

    update

--- custom.js
+++ custom.js
@@ -10,26 +10,6 @@
 		setTimeout(() => {
 			function setProjectId(_e) {
 				console.log(_e);
-				//console.log(baseUrl + `/products/${ _e.data.source.product.id }/metafields.json`)
-				//fetch(baseUrl + `products/${_e.data.source.product.id}/metafields.json`, {
-				//	method: 'POST',
-				//	headers: {
-				//		'X-Shopify-Access-Token': token,
-				//		'Accept': "application/json",
-				//		"Content-Type": "application/json"
-				//	},
-				//	body: JSON.stringify({
-				//		"metafield": {
-				//			"namespace": "inventory",
-				//			"key": "designImage",
-				//			"value": `${_e.data.previews[0]}`,
-				//			"type": "single_line_text_field"
-				//		}
-				//	})
-				//})
-				//	.then(response => response.json())
-				//	.then(data => console.log(data));
-
 				if (document.getElementById('_pitchprint')) {
 					var pp = document.getElementById('_pitchprint').value;
 					var design = document.getElementById('designurl');
@@ -46,7 +26,7 @@
 			//}
 			if (window.ppclient) {
 				window.ppclient.on('project-saved', setProjectId);
-				ppclient.on('lib-ready', setProjectId);
+				window.ppclient.on('lib-ready', setProjectId);
 			}
 			
 		}, 2000)
